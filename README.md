68a110c9abea40c95f0a5ed3dd9ebe09ceb4671d
James Logan <ja_logan@hotmail.com>
2018-03-24 21:49:37 -0700

    Initial upload
    Initial upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-task-card-app
+# angular2 Task card app
+
+from the course on [SitePoint](https://www.sitepoint.com/premium/courses/building-a-task-card-app-with-angular-2-2986/)
---
+++ app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from "@angular/core";
+
+import { Task } from "./model/task";
+
+@Component ({
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
+    styleUrls: [ 'app.component.css' ]
+})
+export class AppComponent {
+    private tasks = [
+        new Task(
+            "buy a monkey",
+            false
+        ),
+        new Task(
+            "Walk the Turtle",
+            false
+        )
+    ]
+    private currentTask = new Task(null, false);
+
+    addTask(){
+        let task = new Task(this.currentTask.content, this.currentTask.completed);
+        this.tasks.push(task);
+    }
+}
